28616551da7f3d7a4440abacdd0a099ccc2f3a73
Arthur Menezes <arthur.menezes@presidencia.gov.br>
2021-01-08 17:53:14 -0300

    Create: Request and Response from Marvel API.

--- app/src/pages/Dashboard/index.tsx
+++ app/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useState } from 'react';
+import { CircularProgress } from '@material-ui/core';
 import { Title, Form, Comics } from './styles';
 import api from '../../services/api';
 import keys from '../../config/keys';
@@ -8,12 +9,29 @@ interface Character {
 
   description: string;
 
+  thumbnail: {
+    path: string;
+  };
+
   id: number;
 }
 
 const Dashboard: React.FC = () => {
   const [newCharacter, setNewCharacter] = useState('');
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [isRequested, setIsRequested] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleCharacterInputEmpty() {
+    alert('O campo de busca nao pode ser vazio');
+  }
+
+  function handleCheckLoading() {
+    if (isLoading === false) {
+      return false;
+    }
+    return true;
+  }
 
   async function handleAddCharacter(
     event: FormEvent<HTMLFormElement>,
@@ -25,55 +43,78 @@ const Dashboard: React.FC = () => {
 
     event.preventDefault();
 
-    let response = await api.get(
-      `v1/public/characters?ts=1&apikey=${keys.api_key}&hash=${keys.hash}&limit=100`,
-    );
-
-    const results = response.data;
-    const { data } = results;
-    let { offset } = data;
-    const { total } = data;
-
-    do {
-      response = await api.get(
-        `v1/public/characters?ts=1&apikey=${keys.api_key}&hash=${keys.hash}&limit=100&offset=${offset}`,
+    if (isRequested === false) {
+      let response = await api.get(
+        `v1/public/characters?ts=1&apikey=${keys.api_key}&hash=${keys.hash}&limit=100`,
       );
 
-      offset += 100;
-    } while (offset < total);
+      const results = response.data;
+      const { data } = results;
+      let { offset } = data;
+      const { total } = data;
+      const allCharacters: ConcatArray<never>[] = [];
+      if (newCharacter.length > 0) {
+        do {
+          setIsLoading(true);
 
-    const character = data.results.map((char: Character) => {
-      const { name, description, id } = char;
+          response = await api.get(
+            `v1/public/characters?ts=1&apikey=${keys.api_key}&hash=${keys.hash}&limit=100&offset=${offset}`,
+          );
+          offset += 100;
 
-      return {
-        name,
-        description,
-        id,
-      };
-    });
+          allCharacters.push(response.data.data.results);
+        } while (offset < total);
 
-    setCharacters([...characters, character]);
+        const flated = [].concat(...allCharacters);
+
+        const foundChar = flated.filter((char: Character) =>
+          char.name.includes(newCharacter),
+        );
+
+        foundChar.map((char: Character) =>
+          setCharacters([...characters, char]),
+        );
+
+        console.log(foundChar);
+
+        setIsLoading(false);
+        setNewCharacter('');
+        setIsRequested(false);
+      } else {
+        handleCharacterInputEmpty();
+      }
+    } else {
+      setIsRequested(true);
+    }
   }
+
   return (
     <>
       <Title>Marvel Characters</Title>
-      <Form onSubmit={(e) => handleAddCharacter(e)} action="">
+      <Form onSubmit={e => handleAddCharacter(e)} action="">
         <input
           value={newCharacter}
-          onChange={(e) => setNewCharacter(e.target.value)}
+          onChange={e => setNewCharacter(e.target.value)}
           type="text"
           placeholder="Digite aqui o nome do personagem"
         />
-        <button type="submit">Pesquisar HQ</button>
+        {handleCheckLoading() ? (
+          <button type="button">
+            {' '}
+            <CircularProgress color="primary" />
+          </button>
+        ) : (
+          <button type="submit">Pesquisar HQ</button>
+        )}
       </Form>
       <Comics>
-        {characters.map((character) => (
+        {characters.map(character => (
           <a key={character.id} href="teste">
             <div>
               <strong>{character.name}</strong>
             </div>
 
-            {/* <img src={character.thumbnail} alt="Thumbanil Personagem" /> */}
+            <img src={character.thumbnail.path} alt={character.name} />
           </a>
         ))}
       </Comics>
--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
 const api = axios.create({
   baseURL: 'https://gateway.marvel.com:443/' || process.env.REACT_APP_BASE_URL,
 });
